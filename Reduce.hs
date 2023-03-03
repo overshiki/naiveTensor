c3f92ae085e0ca291715c25bd4a15f44697e37ce
Le <le.niu@xtalpi.com>
2023-03-03 18:49:41 +0800

    update

--- Reduce.hs
+++ Reduce.hs
@@ -1,15 +1,39 @@
-module NaiveTensor.Reduce (reduceOnce) where 
+module NaiveTensor.Reduce (reduceAll, reduceOnce, reduceHead) where 
 
 import NaiveTensor.NTensor 
 
-reduceOnce :: ([a] -> a) -> (NaiveTensor a) -> (NaiveTensor a)
-reduceOnce reduceFunc (Tensor ax@((Leaf x):xs)) = Leaf (reduceFunc (map get_content ax))
+
+reduceAll :: (a -> a -> a) -> (NaiveTensor a) -> a
+reduceAll func (Tensor ax@((Leaf x):xs)) = foldl func x (map get_content xs)
+reduceAll func (Tensor ax@(x@(Tensor _x):xs)) = foldl func (reduceAll func x) (map (reduceAll func) xs)
+
+foldReduce :: (a -> a -> a) -> [a] -> a 
+foldReduce func (x:xs) = foldl func x xs 
+
+reduceOnce :: (a -> a -> a) -> (NaiveTensor a) -> (NaiveTensor a)
+reduceOnce reduceFunc (Tensor ax@((Leaf x):xs)) = Leaf (foldReduce reduceFunc (map get_content ax))
 reduceOnce reduceFunc (Tensor ax@((Tensor x):xs)) = Tensor (map (reduceOnce reduceFunc) ax)
+
+reduceHead :: (a -> a -> a) -> (NaiveTensor a) -> (NaiveTensor a)
+reduceHead reduceFunc ta@(Tensor ax@((Leaf x):xs)) = ta 
+reduceHead reduceFunc (Tensor ax@((Tensor x):xs)) = Tensor (map (Leaf . (reduceAll reduceFunc)) ax)
+
+
+-- reduceOnce :: ([a] -> a) -> (NaiveTensor a) -> (NaiveTensor a)
+-- reduceOnce reduceFunc (Tensor ax@((Leaf x):xs)) = Leaf (reduceFunc (map get_content ax))
+-- reduceOnce reduceFunc (Tensor ax@((Tensor x):xs)) = Tensor (map (reduceOnce reduceFunc) ax)
+
+-- reduceAll :: ([a] -> a) -> (NaiveTensor a) -> (NaiveTensor a)
+-- reduceAll reduceFunc 
+
+-- reduceHead :: ([a] -> a) -> (NaiveTensor a) -> (NaiveTensor a) 
+-- reduceHead reduceFunc 
 
 
 main :: IO ()
 main = do 
     let nnt = Tensor [(range 1 10), (range 11 20)]
-    print $ reduceOnce sum nnt
+    print $ reduceOnce (+) nnt
     let nnt = ones [2,2,2]
-    print $ reduceOnce sum nnt
+    print $ reduceOnce (+) nnt
+    print $ reduceHead (+) nnt
--- Statistic.hs
+++ Statistic.hs
@@ -5,17 +5,17 @@ module NaiveTensor.Statistic (
             ) where 
 
 import NaiveTensor.NTensor 
+import NaiveTensor.Reduce
 
-
-nested_apply :: Ord a => (a -> a -> a) -> (NaiveTensor a) -> a
-nested_apply func (Tensor ax@((Leaf x):xs)) = foldl func x (map get_content xs)
-nested_apply func (Tensor ax@(x@(Tensor _x):xs)) = foldl func (nested_apply func x) (map (nested_apply func) xs)
+-- nested_apply :: Ord a => (a -> a -> a) -> (NaiveTensor a) -> a
+-- nested_apply func (Tensor ax@((Leaf x):xs)) = foldl func x (map get_content xs)
+-- nested_apply func (Tensor ax@(x@(Tensor _x):xs)) = foldl func (nested_apply func x) (map (nested_apply func) xs)
 
 nt_min :: Ord a => (NaiveTensor a) -> a 
-nt_min = nested_apply min 
+nt_min = reduceAll min 
 
 nt_max :: Ord a => (NaiveTensor a) -> a
-nt_max = nested_apply max 
+nt_max = reduceAll max 
 
 
 main :: IO ()
